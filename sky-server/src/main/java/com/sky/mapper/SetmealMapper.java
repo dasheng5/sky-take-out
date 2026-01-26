a8b9cca0260f7f49e1ea7d97bb1082807a3817d4
DaSheng <13140313876@163.com>
2026-01-26 13:54:26 +0800

    客户端套餐查询，展示代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -5,10 +5,13 @@ import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -43,4 +46,17 @@ public interface SetmealMapper {
     * */
     @AutoFill(OperationType.UPDATE)
     void update(Setmeal setmeal);
+
+    /*
+    * 根据条件动态查询套餐
+    * */
+    List<Setmeal> list(Setmeal setmeal);
+
+    /*
+    * 查询套餐内菜品信息
+    * */
+    @Select("select sd.name, sd.copies, d.image, d.description " +
+            "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
+            "where sd.setmeal_id = #{setmealId}")
+    List<DishItemVO> getDishItemBySetmealId(Long id);
 }
