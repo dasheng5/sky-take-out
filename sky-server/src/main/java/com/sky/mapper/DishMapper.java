c38882e98d2016a50da9387222f87c7c9d04ce59
DaSheng <13140313876@163.com>
2026-01-26 13:07:37 +0800

    套餐起售停售业务开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -62,4 +62,9 @@ public interface DishMapper {
     * 根据分类条件和起售状态条件查询菜品
     * */
     List<Dish> list(Dish dish);
+    /*
+    * 根据套餐id查询菜品
+    * */
+    @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
+    List<Dish> getBySetmealId(Long id);
 }
