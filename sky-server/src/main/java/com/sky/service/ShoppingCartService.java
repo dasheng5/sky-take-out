a58c594877c14775f99a1c3b8fd6a32cde2bc8c8
DaSheng <13140313876@163.com>
2026-01-27 19:18:28 +0800

    添加购物车代码开发

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,17 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+
+/**
+ * @ author Mr.Gu
+ * @ (￣＾￣)
+ * @ date: 2026/1/27
+ * @ time: 18:11
+ * @ description:
+ */
+public interface ShoppingCartService {
+    /*
+    * 添加购物车
+    * */
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
